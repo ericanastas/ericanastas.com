882fbafe8d96091a4ee3d3be5663246097bc6e2e
Eric Anastas <aireq303@gmail.com>
2024-04-04 08:10:44 -0700

    Removed HeroProject

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Container from "@/app/_components/container";
-import { HeroProject } from "@/app/_components/hero-project";
 import { Intro } from "@/app/_components/intro";
 import { ProjectList } from "@/app/_components/project-list";
 import { getAllProjects as getAllProjects } from "@/lib/api";
@@ -7,22 +6,11 @@ import { getAllProjects as getAllProjects } from "@/lib/api";
 export default function Index() {
   const allProjects = getAllProjects();
 
-  const heroProject = allProjects[0];
-
-  const moreProjects = allProjects.slice(1);
-
   return (
     <main>
       <Container>
         <Intro />
-        <HeroProject
-          title={heroProject.title}
-          coverImage={heroProject.coverImage}
-          date={heroProject.date}
-          slug={heroProject.slug}
-          excerpt={heroProject.excerpt}
-        />
-        {moreProjects.length > 0 && <ProjectList projects={moreProjects} />}
+        {allProjects.length > 0 && <ProjectList projects={allProjects} />}
       </Container>
     </main>
   );
