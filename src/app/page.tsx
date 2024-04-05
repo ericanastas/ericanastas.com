dbfee86ba0761e2f7713b497aa77e4d1c28c069e
Eric Anastas <aireq303@gmail.com>
2024-04-05 15:44:33 -0700

    Added social media links to home page #29

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Index() {
   return (
@@ -13,6 +14,98 @@ export default function Index() {
           src={"/images/headshot_bw.jpg"}
           alt="Head shot"
         />
+      </div>
+
+      <div className="flex justify-center	">
+        <div className="grid grid-cols-5 gap-x-2">
+          <Link href={"https://www.instagram.com/ericanastas/"}>
+            <Image
+              height={64}
+              width={64}
+              src={"/images/picons-social/104425_instagram_icon.png"}
+              alt="Instagram"
+            />
+          </Link>
+          <Link href={"https://soundcloud.com/eric-anastas"}>
+            <Image
+              height={64}
+              width={64}
+              src={"/images/picons-social/104428_soundcloud_icon.png"}
+              alt="Soundcloud"
+            />
+          </Link>
+          <Link
+            href={"https://www.youtube.com/channel/UCD05Q0S8a6FBz-xti77NCIw"}
+          >
+            <Image
+              height={64}
+              width={64}
+              src={"/images/picons-social/104445_video_youtube_icon.png"}
+              alt="YouTube"
+            />
+          </Link>
+          <Link href={"https://vimeo.com/ericanastas"}>
+            <Image
+              height={64}
+              width={64}
+              src={"/images/picons-social/104450_video_vimeo_icon.png"}
+              alt="Vimeo"
+            />
+          </Link>
+          <Link href={"https://www.linkedin.com/in/ericanastas/"}>
+            <Image
+              height={64}
+              width={64}
+              src={"/images/picons-social/104452_linkedin_icon.png"}
+              alt="Linkedin"
+            />
+          </Link>
+          <Link href={"https://www.facebook.com/eric.anastas"}>
+            <Image
+              height={64}
+              width={64}
+              src={
+                "/images/picons-social/104458_facebook_social media_fb_social_icon.png"
+              }
+              alt="Facebook"
+            />
+          </Link>
+          <Link href={"https://twitter.com/EricAnastas"}>
+            <Image
+              height={64}
+              width={64}
+              src={"/images/picons-social/104461_twitter_icon.png"}
+              alt="Twitter"
+            />
+          </Link>
+          <Link href={"https://github.com/ericanastas"}>
+            <Image
+              height={64}
+              width={64}
+              src={"/images/picons-social/394187_github_icon.png"}
+              alt="GitHub"
+            />
+          </Link>
+          <Link href={"https://stackoverflow.com/users/88427/eric-anastas"}>
+            <Image
+              height={64}
+              width={64}
+              src={
+                "/images/picons-social/394191_overflow_stack_stackoverflow_icon.png"
+              }
+              alt="Stack Overflow"
+            />
+          </Link>
+
+          <Link href={"https://www.reddit.com/user/aireq/"}>
+            <Image
+              height={64}
+              width={64}
+              src={"/images/picons-social/1181212_reddit_icon.png"}
+              alt="reddit"
+            />
+          </Link>
+        </div>
       </div>
     </main>
   );
