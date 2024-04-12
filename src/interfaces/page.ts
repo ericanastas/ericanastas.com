c4110b8eb9980d0fd3e5bb6b5b9b1b904a32f04d
Eric Anastas <aireq303@gmail.com>
2024-04-12 11:12:52 -0700

    Added comments to page and project types

--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -1,8 +1,19 @@
 export type Page = {
+  /**Url slog */
   slug: string;
+
+  //**Title of content */
   title: string;
+
+  /**markdown content */
   content: string;
+
+  //**HTMM content generated from `content` */
   html: string;
+
+  /**Indicates if the content is a draft and should be hidden in production */
   draft?: boolean;
+
+  /**Url of the content */
   url: string;
 };
--- src/interfaces/project.ts
+++ src/interfaces/project.ts
@@ -3,11 +3,27 @@ import { Category } from "./category";
 import type { Page } from "./page";
 
 export interface Project extends Page {
+  /**Date of project in YYYY-MM-DD */
   date: string;
+
+  /**URL to cover image */
   coverImage?: string;
+
+  /**Short one or two sentance summary of project */
   summary: string;
+
+  /**Tags */
   tags: Tag[];
+
+  /**Mark the project as featured to cause it to sort to the top */
   featured?: boolean;
+
+  /**Project category */
   category: Category;
+
+  /**Hide the project */
   hide?: boolean;
+
+  /**Git repo slug*/
+  repo?: string;
 }
