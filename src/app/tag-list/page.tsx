be491931fc24f1cabdf1e233f6fac4972bb8c80d
Eric Anastas <aireq303@gmail.com>
2024-04-27 18:47:22 -0700

    Added /tag-list page

---
+++ src/app/tag-list/page.tsx
@@ -0,0 +1,38 @@
+import { getAllTags } from "@/lib/projectsApi";
+import TagChipList from "../_components/tag-chip-list";
+
+import PageTitle from "../_components/page-title";
+import Header from "../_components/header";
+import Footer from "../_components/footer";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export default async function Tags() {
+  let allTags = await getAllTags();
+
+  return (
+    <>
+      <Header />
+      <main>
+        <article className="mb-16 max-w-4xl mx-auto">
+          <div className="mb-12">
+            <PageTitle>Tag List</PageTitle>
+          </div>
+
+          <ul>
+            {allTags.map((tag) => (
+              <li>
+                <Link href={tag.url}>{tag.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export const metadata: Metadata = {
+  title: "Tags",
+};
