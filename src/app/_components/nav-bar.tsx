eaafe3e055214c59ae5ad73b8f74d642d8a55a8c
Eric Anastas <aireq303@gmail.com>
2024-07-10 20:21:17 -0700

    Replaced link to pdf in NavBar with link to /resume

--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -3,9 +3,9 @@ import NavLink from "./nav-link";
 export function NavBar() {
   return (
     <nav className="flex gap-4">
+      <NavLink href="/resume" text="Resume" />
       <NavLink href="/projects" text="Projects" />
       <NavLink href="/skills" text="Skills" />
-      <NavLink href="/eric-anastas-resume.pdf" text="Resume" />
     </nav>
   );
 }
