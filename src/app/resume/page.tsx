84da01bc81d82436771e1b13209d0cdd75d94056
Eric Anastas <aireq303@gmail.com>
2024-07-13 06:31:58 -0700

    Updated PDF copy or resume and added download button to /resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,6 +5,7 @@ import { Metadata } from "next";
 
 import { AEC_RESUME } from "./aec-resume";
 import PageTitle from "../_components/page-title";
+import IconDownload from "../_components/icons/IconDownload";
 
 export default async function ResumePage() {
   return (
@@ -13,6 +14,16 @@ export default async function ResumePage() {
       <main>
         <article>
           <PageTitle>Resume</PageTitle>
+          <div className="print:hidden flex justify-center pb-4">
+            <a href="/eric-anastas-resume.pdf">
+              <div className="button-dark px-3.5 py-2.5 flex items-center gap-2 cursor-pointer">
+                <div>
+                  <IconDownload fontSize={24} />
+                </div>
+                <div>Download PDF</div>
+              </div>
+            </a>
+          </div>
           <div className="mx-auto border border-gray-400 max-w-[816px] min-h-[1056px] shadow-sm p-[48px]">
             <ResumeComponent resume={AEC_RESUME} />
           </div>
