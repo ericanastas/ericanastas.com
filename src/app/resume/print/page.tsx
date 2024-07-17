22420feee9b1f7c3e4477e707176e0d26f7a48cd
Eric Anastas <aireq303@gmail.com>
2024-07-17 00:00:17 -0700

    updated resume print page to make urls absolute with base url: https://ericanastas.com

--- src/app/resume/print/page.tsx
+++ src/app/resume/print/page.tsx
@@ -6,8 +6,37 @@ import { SE_RESUME } from "../se-resume";
 import IconPrinterFill from "@/app/_components/icons/IconPrinterFill";
 
 import "./print-styles.scss";
+import { Company, Resume } from "@/interfaces/resume";
+
+const baseUrl = "https://ericanastas.com";
+
+function absUrl(url?: string): string | undefined {
+  if (url) {
+    let urlObj = new URL(url, baseUrl);
+    return urlObj.toString();
+  }
+  return undefined;
+}
 
 export default async function ResumePage() {
+  let absoluteUrlResume: Resume = {
+    contactInfo: SE_RESUME.contactInfo,
+    education: SE_RESUME.education.map((e) => ({ ...e, url: absUrl(e.url) })),
+    projects: SE_RESUME.projects.map((p) => ({ ...p, url: absUrl(p.url)! })),
+    skills: SE_RESUME.skills.map((sg) => ({
+      ...sg,
+      skills: sg.skills.map((s) => ({ ...s, url: absUrl(s.url) })),
+    })),
+    workExperience: SE_RESUME.workExperience.map<Company>((c) => ({
+      ...c,
+      url: absUrl(c.url)!,
+      positions: c.positions.map((p) => ({
+        ...p,
+        achievements: p.achievements.map((a) => ({ ...a, url: absUrl(a.url) })),
+      })),
+    })),
+  };
+
   function handlePrintClick() {
     window.print();
   }
@@ -24,7 +53,7 @@ export default async function ResumePage() {
             Print
           </div>
         </div>
-        <ResumeComponent resume={SE_RESUME} />
+        <ResumeComponent resume={absoluteUrlResume} />
       </main>
     </>
   );
